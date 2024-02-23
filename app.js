ba24538904155e87d13a76130c487f7a285d6d96
siyaphe <siyapdje@yahoo.fr>
2024-02-23 08:50:01 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+// keyword --- name --- () => {}
+const changeButton = ()=> {
+    //this is where i put my instructions
+    let container = document.querySelector('.lowContainer')
+    container.computedStyle.background="blue"
+    }
